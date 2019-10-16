447139e38f7129a62cadfc569663cdef93ca9c9d
Danilo Hernandez <dhernandez0032@gmail.com>
2019-10-16 12:21:56 -0600

    update 16/10/2019 12:20

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -71,10 +71,10 @@
           </div>
         </li>
         @endcan
-        <hr class="sidebar-divider d-none d-md-block">
-        <div class="text-center d-none d-md-inline">
+        <!--hr class="sidebar-divider d-none d-md-block"-->
+        <!--div class="text-center d-none d-md-inline">
           <button class="rounded-circle border-0" id="sidebarToggle"></button>
-        </div>
+        </div-->
       </ul>
 
       <div id="content-wrapper" class="d-flex flex-column">
